70460ecae2caa0dfedde8c9c83ae84b6c2905667
hosn <xuhosn@gmail.com>
2016-02-28 18:08:44 +0800

    first push

---
+++ README.md
@@ -0,0 +1,14 @@
+# 局域网下共享文件的小工具
+
+## 安装
+
+```bash
+$ npm install qrshare -g
+```
+---
+## 用法
+
+```bash
+$ qrshare <file_path>
+```
+然后用手机扫描二维码即可下载文件.
---
+++ app.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var os = require('os');
+var http = require('http');
+var path = require('path');
+var express = require('express');
+var qrcode = require('qrcode-terminal');
+var ifaces = os.networkInterfaces();
+var wlan = ifaces['wlan0'] || ifaces['Wi-Fi'];
+var address = wlan.filter(ad => ad.family === 'IPv4')[0].address;
+var app = express();
+var filePath = process.argv[2];
+if (!filePath) {
+  console.log(`-> run qrshare <file_path>`);
+  process.exit(0);
+}
+var fileName = path.basename(filePath);
+var server = http.createServer(app).listen(0, address, () => {
+  console.log(`The file is >> ${fileName} <<`);
+  console.log(`The url is http:\/\/${address}:${server.address().port}/${fileName}`);
+  qrcode.generate(`http:\/\/${address}:${server.address().port}/${fileName}`, code => {
+    console.log(code);
+  });
+});
+
+app.get(`/${fileName}`, (req, res, next) => {
+  res.download(filePath, fileName, (err) => {
+    if (err) {
+      console.log('Cant find the file!');
+      res.end('Cant find the file!');
+    }
+    setTimeout(() => {
+      console.log('Done!');
+      process.exit(0);
+    }, 3000);
+  });
+});
